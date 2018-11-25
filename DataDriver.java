710e739681772e34657fa6432d70ec1269c7e2a9
TobuHasu <44510028+TobuHasu@users.noreply.github.com>
2018-11-25 18:35:41 -0500

    Update DataDriver.java

--- DataDriver.java
+++ DataDriver.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import core.data.*;
 
 public class DataDriver{
@@ -8,18 +9,28 @@ public class DataDriver{
    
    //main method
    public static void main(String[] args){
+   Scanner kb = new Scanner(System.in);
       
       //load the data source
       ds.load();
       
       //reveal the structure of the JSON file
       testSource();
+      
+      //Total number of docks? How many are test stations?
+      int dockCount = 0;
+      int testCount = 0;
+      for(int i = 0; i < bikeShare.length; i++){
+         if (bikeShare[i].getTestStation().equals true){
+         testCount++;
+      }
    }
    
    public static ChicagoBikeShare[] fetchAll(){
-      ChicagoBikeShare[] bikeShare = ds.fetchArray("ChicagoBikeShare", "latitude", "longitude", "availableDocks", "availableBikes", "totalDocks", "testStation");
+      ChicagoBikeShare[] bikeShare = ds.fetchArray("ChicagoBikeShare", "latitude", "longitude", "id", "availableDocks", "availableBikes", "totalDocks", "testStation");
       return bikeShare;
    }
+   ChicagoBikeShare[] bikeShare = fetchAll();
    
    //display the JSON structure for available data
    public static void testSource(){
