0887b98e9a0d1c6b271f5310425911ae52dfdfd4
TobuHasu <44510028+TobuHasu@users.noreply.github.com>
2018-11-26 18:35:11 -0500

    Update DataDriver.java

--- DataDriver.java
+++ DataDriver.java
@@ -13,13 +13,36 @@ public class DataDriver{
       ds.load();
       testSource();
       ChicagoBikeShare[] bikeShare = fetchAll();
+
+      //Total number of docks? How many docks are available?
+      int dockCount = 0;
+      int aDockCount = 0;
+      for(int i = 0; i < bikeShare.length; i++){
+         dockCount += bikeShare[i].getTotalDocks();
+         aDockCount += bikeShare[i].getAvailableDocks();
+      }
+      System.out.println("Total number of docks: " + dockCount);
+      System.out.println("Total number of available docks: " + aDockCount);
       
-      //Total number of docks? How many are test stations?
+      //Total number of bikes available
+      int bikeCount = 0;
+      for(int j = 0; j < bikeShare.length; j++){
+         bikeCount += bikeShare[j].getAvailableBikes();
+      }
+      System.out.println("Total number of available bikes: " + bikeCount);
       
+      //Prompt user to type in a station id, print the longitude/latitude of that station
+      System.out.print("Please enter a station id number from 2 to 663: ");
+      int idNum = kb.nextInt();
+      for(int num = 0; num < idNum; num++){
+         if(idNum <= 663 && idNum >= 2){
+            System.out.println();
+         }
+      }  
    }
    
    public static ChicagoBikeShare[] fetchAll(){
-      ChicagoBikeShare[] bikeShare = ds.fetchArray("ChicagoBikeShare", "stationBeanList/latitude", "stationBeanList/longitude", "stationBeanList/id", "stationBeanList/availableDocks", "stationBeanList/availableBikes", "stationBeanList/totalDocks", "stationBeanList/testStation");
+      ChicagoBikeShare[] bikeShare = ds.fetchArray("ChicagoBikeShare", "stationBeanList/latitude", "stationBeanList/longitude", "stationBeanList/id", "stationBeanList/availableDocks", "stationBeanList/availableBikes", "stationBeanList/totalDocks");
       return bikeShare;
    }
    //display the JSON structure for available data
