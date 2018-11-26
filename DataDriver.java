f562bc76c8d41885a09317e8754a405a1cca5e12
TobuHasu <44510028+TobuHasu@users.noreply.github.com>
2018-11-26 16:37:46 -0500

    Update DataDriver.java

--- DataDriver.java
+++ DataDriver.java
@@ -7,32 +7,21 @@ public class DataDriver{
    public static String url = "https://feeds.divvybikes.com/stations/stations.json";
    public static DataSource ds = DataSource.connect(url);
    
-   //main method
+   //Main Method
    public static void main(String[] args){
-   Scanner kb = new Scanner(System.in);
-      
-      //load the data source
+      Scanner kb = new Scanner(System.in);
       ds.load();
-      
-      //reveal the structure of the JSON file
       testSource();
+      ChicagoBikeShare[] bikeShare = fetchAll();
       
       //Total number of docks? How many are test stations?
-      int dockCount = 0;
-      int testCount = 0;
-      for(int i = 0; i < bikeShare.length; i++){
-         if (bikeShare[i].getTestStation().equals true){
-         testCount++;
-      }
+      
    }
-}
    
    public static ChicagoBikeShare[] fetchAll(){
       ChicagoBikeShare[] bikeShare = ds.fetchArray("ChicagoBikeShare", "latitude", "longitude", "id", "availableDocks", "availableBikes", "totalDocks", "testStation");
       return bikeShare;
    }
-   ChicagoBikeShare[] bikeShare = fetchAll();
-   
    //display the JSON structure for available data
    public static void testSource(){
       ds.printUsageString();
